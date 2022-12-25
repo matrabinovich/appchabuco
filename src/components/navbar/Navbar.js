0258fa7482a900d4ffa96dad7030a085f803323b
matrabinovich <mat.rabinovich@gmail.com>
2022-12-25 15:47:37 -0300

    cambios

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav>
+            <h1>Chabuco</h1>
+            <div>
+                <button>Categoria 1</button>
+                <button>Categoria 2</button>
+                <button>Categoria 3</button>
+                <button>Categoria 4</button>
+            </div>
+        </nav>
+    
+    )
+}
+
+
+export default Navbar
